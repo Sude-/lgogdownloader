d31a5c05b7828f9c7a68af1bf3efee76f81341cd
Sude <lgogdownloader@gmail.com>
2014-01-05 16:06:19 +0200

    Version 2.11

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@
 #include <boost/filesystem.hpp>
 #include <boost/program_options.hpp>
 
-#define VERSION_NUMBER "2.10"
+#define VERSION_NUMBER "2.11"
 
 #ifndef VERSION_STRING
 #   define VERSION_STRING "LGOGDownloader " VERSION_NUMBER
