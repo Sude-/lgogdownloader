5dd8332b9eed9f84c2d4abe47549bfabab61ac46
Sude <lgogdownloader@gmail.com>
2014-08-18 07:59:04 +0300

    Add --no-deb and --no-targz command line options

--- main.cpp
+++ main.cpp
@@ -117,6 +117,8 @@ int main(int argc, char *argv[])
         bool bNoDLC = false;
         bool bNoRemoteXML = false;
         bool bNoSubDirectories = false;
+        bool bNoDeb = false;
+        bool bNoTarGz = false;
         // Commandline options (no config file)
         options_cli_no_cfg.add_options()
             ("help,h", "Print help message")
@@ -148,6 +150,8 @@ int main(int argc, char *argv[])
             ("no-patches", bpo::value<bool>(&bNoPatches)->zero_tokens()->default_value(false), "Don't download/list/repair patches")
             ("no-language-packs", bpo::value<bool>(&bNoLanguagePacks)->zero_tokens()->default_value(false), "Don't download/list/repair language packs")
             ("no-dlc", bpo::value<bool>(&bNoDLC)->zero_tokens()->default_value(false), "Don't download/list/repair DLCs")
+            ("no-deb", bpo::value<bool>(&bNoDeb)->zero_tokens()->default_value(false), "Don't download/list/repair deb packages")
+            ("no-targz", bpo::value<bool>(&bNoTarGz)->zero_tokens()->default_value(false), "Don't download/list/repair tarballs")
             ("cover", bpo::value<bool>(&config.bCover)->zero_tokens()->default_value(false), "Download cover images")
             ("no-remote-xml", bpo::value<bool>(&bNoRemoteXML)->zero_tokens()->default_value(false), "Don't use remote XML for repair")
             ("no-unicode", bpo::value<bool>(&bNoUnicode)->zero_tokens()->default_value(false), "Don't use Unicode in the progress bar")
@@ -204,8 +208,21 @@ int main(int argc, char *argv[])
                     std::getline(ifs, line);
                     lines.push_back(std::move(line));
                 }
+                if (bNoDeb)
+                    lines.push_back("Rp .*\\.deb$");
+                if (bNoTarGz)
+                    lines.push_back("Rp .*\\.tar\\.gz$");
                 config.blacklist.initialize(lines);
             }
+        }
+        else if (bNoDeb || bNoTarGz)
+        {
+            std::vector<std::string> lines;
+            if (bNoDeb)
+                lines.push_back("Rp .*\\.deb$");
+            if (bNoTarGz)
+                lines.push_back("Rp .*\\.tar\\.gz$");
+            config.blacklist.initialize(lines);
         }
 
         if (vm.count("help"))
