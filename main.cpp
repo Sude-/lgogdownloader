8caaf30f169e5efea960af009e737a862dc81185
Sude <lgogdownloader@gmail.com>
2015-05-17 19:04:59 +0300

    Version 2.23

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@
 #include <boost/filesystem.hpp>
 #include <boost/program_options.hpp>
 
-#define VERSION_NUMBER "2.22"
+#define VERSION_NUMBER "2.23"
 
 #ifndef VERSION_STRING
 #   define VERSION_STRING "LGOGDownloader " VERSION_NUMBER
