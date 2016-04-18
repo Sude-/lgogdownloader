2538825fb2d97335a2b3ae513733497dcaf14f7a
Sude <lgogdownloader@gmail.com>
2016-04-18 20:39:31 +0300

    Add missing WTFPL license text to gameDetails and gameFile classes

--- src/gamefile.cpp
+++ src/gamefile.cpp
@@ -1,3 +1,9 @@
+/* This program is free software. It comes without any warranty, to
+ * the extent permitted by applicable law. You can redistribute it
+ * and/or modify it under the terms of the Do What The Fuck You Want
+ * To Public License, Version 2, as published by Sam Hocevar. See
+ * http://www.wtfpl.net/ for more details. */
+
 #include "gamefile.h"
 
 gameFile::gameFile(const int& t_updated, const std::string& t_id, const std::string& t_name, const std::string& t_path, const std::string& t_size, const unsigned int& t_language, const unsigned int& t_platform, const int& t_silent)
