Sude-/lgogdownloader

commit 33fb004cdd8ccbcb08b1f5ad9b62b1fe4801941d

Move GOG website related members of Downloader class to new Website class
Downloader::HTTP_Login --> Website::Login
Downloader::getGames --> Website::getGames
Downloader::getFreeGames --> Website::getFreeGames
Downloader::getGameDetailsJSON --> Website::getGameDetailsJSON
Getting wishlist details is split from Downloader::showWishlist to Website::getWishlistItems function

include/website.h
@@ -1,0 +1,38 @@
+/* This program is free software. It comes without any warranty, to
+ * the extent permitted by applicable law. You can redistribute it
+ * and/or modify it under the terms of the Do What The Fuck You Want
+ * To Public License, Version 2, as published by Sam Hocevar. See
+ * http://www.wtfpl.net/ for more details. */
+
+#ifndef WEBSITE_H
+#define WEBSITE_H
+
+#include "config.h"
+#include "util.h"
+#include <curl/curl.h>
+#include <json/json.h>
+#include <fstream>
+
+class Website
+{
+    public:
+        Website(Config &conf);
+        int Login(const std::string& email, const std::string& password);
+        std::string getResponse(const std::string& url);
+        Json::Value getGameDetailsJSON(const std::string& gameid);
+        std::vector<gameItem> getGames();
+        std::vector<gameItem> getFreeGames();
+        std::vector<wishlistItem> getWishlistItems();
+        bool IsLoggedIn();
+        virtual ~Website();
+    protected:
+    private:
+        static size_t writeMemoryCallback(char *ptr, size_t size, size_t nmemb, void *userp);
+        CURL* curlhandle;
+        Config config;
+        bool IsloggedInSimple();
+        bool IsLoggedInComplex(const std::string& email);
+        int retries;
+};
+
+#endif // WEBSITE_H

CMakeLists.txt
@@ -12,7 +12,7 @@ find_package(Boost
   program_options
   date_time
   )
-find_package(CURL REQUIRED)
+find_package(CURL 7.17.1 REQUIRED)
 find_package(OAuth REQUIRED)
 find_package(Jsoncpp REQUIRED)
 find_package(Htmlcxx REQUIRED)
@@ -22,6 +22,7 @@ find_package(Rhash REQUIRED)
 file(GLOB SRC_FILES
   main.cpp
   src/api.cpp
+  src/website.cpp
   src/downloader.cpp
   src/progressbar.cpp
   src/util.cpp

include/downloader.h
@@ -24,6 +24,7 @@
 #include "config.h"
 #include "api.h"
 #include "progressbar.h"
+#include "website.h"
 #include <curl/curl.h>
 #include <json/json.h>
 #include <ctime>
@@ -45,14 +46,6 @@ class Timer
         ~Timer() {};
     private:
         struct timeval last_update;
-};
-
-class gameItem {
-    public:
-        std::string name;
-        std::string id;
-        std::vector<std::string> dlcnames;
-        Json::Value gamedetailsjson;
 };
 
 class Downloader
@@ -91,11 +84,7 @@ class Downloader
         int loadGameDetailsCache();
         int saveGameDetailsCache();
         std::vector<gameDetails> getGameDetailsFromJsonNode(Json::Value root, const int& recursion_level = 0);
-        int HTTP_Login(const std::string& email, const std::string& password);
-        std::vector<gameItem> getGames();
-        std::vector<gameItem> getFreeGames();
         std::vector<gameFile> getExtrasFromJSON(const Json::Value& json, const std::string& gamename);
-        Json::Value getGameDetailsJSON(const std::string& gameid);
         std::string getSerialsFromJSON(const Json::Value& json);
         void saveSerials(const std::string& serials, const std::string& filepath);
         std::string getChangelogFromJSON(const Json::Value& json);
@@ -106,7 +95,7 @@ class Downloader
         static size_t writeData(void *ptr, size_t size, size_t nmemb, FILE *stream);
         static size_t readData(void *ptr, size_t size, size_t nmemb, FILE *stream);
 
-
+        Website *gogWebsite;
         API *gogAPI;
         std::vector<gameItem> gameItems;
         std::vector<gameDetails> games;

include/util.h
@@ -43,6 +43,30 @@ struct gameSpecificConfig
     std::vector<unsigned int> vPlatformPriority;
 };
 
+struct gameItem
+{
+    std::string name;
+    std::string id;
+    std::vector<std::string> dlcnames;
+    Json::Value gamedetailsjson;
+};
+
+struct wishlistItem
+{
+    std::string title;
+    unsigned int platform;
+    std::vector<std::string> tags;
+    time_t release_date_time;
+    std::string currency;
+    std::string price;
+    std::string discount_percent;
+    std::string discount;
+    std::string store_credit;
+    std::string url;
+    bool bIsBonusStoreCreditIncluded;
+    bool bIsDiscounted;
+};
+
 namespace Util
 {
     std::string makeFilepath(const std::string& directory, const std::string& path, const std::string& gamename, std::string subdirectory = "", const unsigned int& platformId = 0, const std::string& dlcname = "");

commit 51026efec099590b907766c3d93cb245635d5390

Fix issue with cache update
Fixes an issue caused by 33fb004cdd8ccbcb08b1f5ad9b62b1fe4801941d
Website handle didn't have updated config which caused overridden values to be ignored

include/website.h
@@ -24,6 +24,7 @@ class Website
         std::vector<gameItem> getFreeGames();
         std::vector<wishlistItem> getWishlistItems();
         bool IsLoggedIn();
+        void setConfig(Config &conf);
         virtual ~Website();
     protected:
     private:

src/downloader.cpp
@@ -2831,6 +2831,7 @@ void Downloader::updateCache()
     config.vLanguagePriority.clear();
     config.vPlatformPriority.clear();
     config.sIgnoreDLCCountRegex = ".*"; // Ignore DLC count for all games because GOG doesn't report DLC count correctly
+    gogWebsite->setConfig(config); // Make sure that website handle has updated config
 
     this->getGameList();
     this->getGameDetails();

src/website.cpp
@@ -701,3 +701,8 @@ std::vector<wishlistItem> Website::getWishlistItems()
 
     return wishlistItems;
 }
+
+void Website::setConfig(Config &conf)
+{
+    this->config = conf;
+}