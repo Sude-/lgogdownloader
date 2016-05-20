Sude-/lgogdownloader

commit 730c1ffe5875d2d7c6d31320f79b0f5be0e85890

Move code from Downloader::getLocalFileHash to Util::getLocalFileHash

src/downloader.cpp
@@ -2555,33 +2555,12 @@ std::string Downloader::getLocalFileHash(const std::string& filepath, const std:
 
     if (config.bAutomaticXMLCreation && !boost::filesystem::exists(local_xml_file) && boost::filesystem::exists(path))
     {
-	std::string xml_directory = config.sXMLDirectory + "/" + gamename;
-	Util::createXML(filepath, config.iChunkSize, xml_directory);
+        std::string xml_directory = config.sXMLDirectory + "/" + gamename;
+        Util::createXML(filepath, config.iChunkSize, xml_directory);
     }
 
-    if (boost::filesystem::exists(local_xml_file))
-    {
-        tinyxml2::XMLDocument local_xml;
-        local_xml.LoadFile(local_xml_file.string().c_str());
-        tinyxml2::XMLElement *fileElemLocal = local_xml.FirstChildElement("file");
-	if (!fileElemLocal && config.bAutomaticXMLCreation)
-	{
-	    std::string xml_directory = config.sXMLDirectory + "/" + gamename;
-	    Util::createXML(filepath, config.iChunkSize, xml_directory);
-	    local_xml.LoadFile(local_xml_file.string().c_str());
-	    fileElemLocal = local_xml.FirstChildElement("file");
-	}
-        if (fileElemLocal)
-        {
-            localHash = fileElemLocal->Attribute("md5");
-	    return localHash;
-        }
-    }
+    localHash = Util::getFileHash(path.string(), RHASH_MD5);
 
-    if (boost::filesystem::exists(path) && boost::filesystem::is_regular_file(path))
-    {
-	localHash = Util::getFileHash(path.string(), RHASH_MD5);
-    }
     return localHash;
 }
 

include/util.h
@@ -88,6 +88,7 @@ namespace Util
     unsigned int getOptionValue(const std::string& str, const std::vector<GlobalConstants::optionsStruct>& options);
     std::string getOptionNameString(const unsigned int& value, const std::vector<GlobalConstants::optionsStruct>& options);
     void parseOptionString(const std::string &option_string, std::vector<unsigned int> &priority, unsigned int &type, const std::vector<GlobalConstants::optionsStruct>& options);
+    std::string getLocalFileHash(const std::string& xml_dir, const std::string& filepath, const std::string& gamename = std::string());
 }
 
 #endif // UTIL_H

src/util.cpp
@@ -540,3 +540,32 @@ void Util::parseOptionString(const std::string &option_string, std::vector<unsig
         type |= value;
     }
 }
+
+std::string Util::getLocalFileHash(const std::string& xml_dir, const std::string& filepath, const std::string& gamename)
+{
+    std::string localHash;
+    boost::filesystem::path path = filepath;
+    boost::filesystem::path local_xml_file;
+    if (!gamename.empty())
+        local_xml_file = xml_dir + "/" + gamename + "/" + path.filename().string() + ".xml";
+    else
+        local_xml_file = xml_dir + "/" + path.filename().string() + ".xml";
+
+    if (boost::filesystem::exists(local_xml_file))
+    {
+        tinyxml2::XMLDocument local_xml;
+        local_xml.LoadFile(local_xml_file.string().c_str());
+        tinyxml2::XMLElement *fileElem = local_xml.FirstChildElement("file");
+
+        if (fileElem)
+        {
+            localHash = fileElem->Attribute("md5");
+        }
+    }
+    else if (boost::filesystem::exists(path) && boost::filesystem::is_regular_file(path))
+    {
+        localHash = Util::getFileHash(path.string(), RHASH_MD5);
+    }
+
+    return localHash;
+}

commit 06fd4d496c295562682cc2200bbc0d913964f47a

Fix issue caused by 730c1ffe5875d2d7c6d31320f79b0f5be0e85890
Util::getFileHash was called instead of Util::getLocalFileHash
This caused librhash error for files that didn't exist

src/downloader.cpp
@@ -2559,7 +2559,7 @@ std::string Downloader::getLocalFileHash(const std::string& filepath, const std:
         Util::createXML(filepath, config.iChunkSize, xml_directory);
     }
 
-    localHash = Util::getFileHash(path.string(), RHASH_MD5);
+    localHash = Util::getLocalFileHash(config.sXMLDirectory, filepath, gamename);
 
     return localHash;
 }