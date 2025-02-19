Sude-/lgogdownloader

commit 367b57ca8905dde5d4a5ba7d97fa9aa9754863b3

Improve subdir filepath template handling
Adds new templates for subdir options
%title%
%title_stripped%
%dlc_title%
%dlc_title_stripped%

include/gamedetails.h
@@ -27,8 +27,10 @@ class gameDetails
         std::vector<gameFile> languagepacks;
         std::vector<gameDetails> dlcs;
         std::string gamename;
+        std::string gamename_basegame;
         std::string product_id;
         std::string title;
+        std::string title_basegame;
         std::string icon;
         std::string serials;
         std::string changelog;
@@ -51,6 +53,8 @@ class gameDetails
     protected:
         void filterListWithPriorities(std::vector<gameFile>& list, const gameSpecificConfig& config);
         void filterListWithType(std::vector<gameFile>& list, const unsigned int& iType);
+        std::string makeFilepath(const gameFile& gf, const DirectoryConfig& dirConf);
+        std::string makeCustomFilepath(const std::string& filename, const gameDetails& gd, const DirectoryConfig& dirConf);
     private:
         std::string serialsFilepath;
         std::string logoFilepath;

include/util.h
@@ -60,8 +60,6 @@ struct wishlistItem
 
 namespace Util
 {
-    std::string makeFilepath(const std::string& directory, const std::string& path, const std::string& gamename, std::string subdirectory = "", const unsigned int& platformId = 0, const std::string& dlcname = "");
-    std::string makeRelativeFilepath(const std::string& path, const std::string& gamename, std::string subdirectory = "");
     std::string getFileHash(const std::string& filename, unsigned hash_id);
     std::string getFileHashRange(const std::string& filepath, unsigned hash_id, off_t range_start = 0, off_t range_end = 0);
     std::string getChunkHash(unsigned char* chunk, uintmax_t chunk_size, unsigned hash_id);

src/downloader.cpp
@@ -2173,6 +2173,9 @@ std::vector<gameDetails> Downloader::getGameDetailsFromJsonNode(Json::Value root
                         fileDetails.language = fileDetailsNode["language"].asUInt();
                         fileDetails.silent = fileDetailsNode["silent"].asInt();
                         fileDetails.gamename = fileDetailsNode["gamename"].asString();
+                        fileDetails.title = fileDetailsNode["title"].asString();
+                        fileDetails.gamename_basegame = fileDetailsNode["gamename_basegame"].asString();
+                        fileDetails.title_basegame = fileDetailsNode["title_basegame"].asString();
                         fileDetails.type = fileDetailsNode["type"].asUInt();
                         fileDetails.galaxy_downlink_json_url = fileDetailsNode["galaxy_downlink_json_url"].asString();
                         if (!fileDetailsNode["version"].empty())
@@ -2446,6 +2449,7 @@ int Downloader::downloadFileWithId(const std::string& fileid_string, const std::
         }
 
         gameDetails gd = gogGalaxy->productInfoJsonToGameDetails(productInfo, dlConf);
+        gd.makeFilepaths(Globals::globalConfig.dirConf);
 
         auto vFiles = gd.getGameFileVector();
         gameFile gf;
@@ -2517,7 +2521,7 @@ int Downloader::downloadFileWithId(const std::string& fileid_string, const std::
         std::string filename, filepath;
         filename = gogGalaxy->getPathFromDownlinkUrl(url, gf.gamename);
         if (output_filepath.empty())
-            filepath = Util::makeFilepath(Globals::globalConfig.dirConf.sDirectory, filename, gf.gamename);
+            filepath = gf.getFilepath();
         else
             filepath = output_filepath;
         std::cout << "Downloading: " << filepath << std::endl;

src/util.cpp
@@ -17,49 +17,6 @@
 #include <tidy.h>
 #include <tidybuffio.h>
 
-/*
-    Create filepath from specified directory and path
-    Remove the leading slash from path if needed
-    Use gamename as base directory if specified
-*/
-std::string Util::makeFilepath(const std::string& directory, const std::string& path, const std::string& gamename, std::string subdirectory, const unsigned int& platformId, const std::string& dlcname)
-{
-    std::string dir = directory + makeRelativeFilepath(path, gamename, subdirectory);
-    Util::filepathReplaceReservedStrings(dir, gamename, platformId, dlcname);
-    return dir;
-}
-
-/* Create filepath relative to download base directory specified in config.
- */
-std::string Util::makeRelativeFilepath(const std::string& path, const std::string& gamename, std::string subdirectory)
-{
-    std::string filepath;
-
-    if (gamename.empty())
-    {
-        if (path.at(0)=='/')
-        {
-            std::string tmp_path = path.substr(1,path.length());
-            filepath = tmp_path;
-        }
-        else
-        {
-            filepath = path;
-        }
-    }
-    else
-    {
-        std::string filename = path.substr(path.find_last_of("/")+1, path.length());
-        if (!subdirectory.empty())
-        {
-            subdirectory = "/" + subdirectory;
-        }
-        filepath = subdirectory + "/" + filename;
-    }
-
-    return filepath;
-}
-
 std::string Util::getFileHash(const std::string& filename, unsigned hash_id)
 {
     unsigned char digest[rhash_get_digest_size(hash_id)];

include/blacklist.h
@@ -29,7 +29,6 @@ class Blacklist
 
         void initialize(const std::vector<std::string>& lines);
         bool isBlacklisted(const std::string& path);
-        bool isBlacklisted(const std::string& path, const std::string& gamename, std::string subdirectory = "");
 
         std::vector<BlacklistItem>::size_type size() const { return blacklist_.size(); }
         bool empty() { return blacklist_.empty(); }

include/gamefile.h
@@ -26,13 +26,16 @@ class gameFile
         std::string size;
         std::string galaxy_downlink_json_url;
         std::string version;
+        std::string title;
+        std::string gamename_basegame = "";
+        std::string title_basegame = "";
         unsigned int platform;
         unsigned int language;
         unsigned int type;
         int score;
         int silent;
         void setFilepath(const std::string& path);
-        std::string getFilepath();
+        std::string getFilepath() const;
         Json::Value getAsJson();
         virtual ~gameFile();
     protected:

include/globalconstants.h
@@ -12,7 +12,7 @@
 
 namespace GlobalConstants
 {
-    const int GAMEDETAILS_CACHE_VERSION = 5;
+    const int GAMEDETAILS_CACHE_VERSION = 6;
     const int ZLIB_WINDOW_SIZE = 15;
 
     struct optionsStruct {const unsigned int id; const std::string code; const std::string str; const std::string regexp;};
@@ -129,14 +129,19 @@ namespace GlobalConstants
     const unsigned int GFTYPE_DLC_EXTRA      = 1 << 5;
     const unsigned int GFTYPE_DLC_PATCH      = 1 << 6;
     const unsigned int GFTYPE_DLC_LANGPACK   = 1 << 7;
+    const unsigned int GFTYPE_CUSTOM_BASE    = 1 << 8;
+    const unsigned int GFTYPE_CUSTOM_DLC     = 1 << 9;
     const unsigned int GFTYPE_DLC = GFTYPE_DLC_INSTALLER | GFTYPE_DLC_EXTRA |
-                                    GFTYPE_DLC_PATCH | GFTYPE_DLC_LANGPACK;
+                                    GFTYPE_DLC_PATCH | GFTYPE_DLC_LANGPACK |
+                                    GFTYPE_CUSTOM_DLC;
     const unsigned int GFTYPE_BASE = GFTYPE_BASE_INSTALLER | GFTYPE_BASE_EXTRA |
-                                     GFTYPE_BASE_PATCH | GFTYPE_BASE_LANGPACK;
+                                     GFTYPE_BASE_PATCH | GFTYPE_BASE_LANGPACK |
+                                     GFTYPE_CUSTOM_BASE;
     const unsigned int GFTYPE_INSTALLER = GFTYPE_BASE_INSTALLER | GFTYPE_DLC_INSTALLER;
     const unsigned int GFTYPE_EXTRA     = GFTYPE_BASE_EXTRA | GFTYPE_DLC_EXTRA;
     const unsigned int GFTYPE_PATCH     = GFTYPE_BASE_PATCH | GFTYPE_DLC_PATCH;
     const unsigned int GFTYPE_LANGPACK  = GFTYPE_BASE_LANGPACK | GFTYPE_DLC_LANGPACK;
+    const unsigned int GFTYPE_CUSTOM    = GFTYPE_CUSTOM_BASE | GFTYPE_CUSTOM_DLC;
 
     const std::vector<GlobalConstants::optionsStruct> INCLUDE_OPTIONS =
     {

main.cpp
@@ -133,7 +133,17 @@ int main(int argc, char *argv[])
     std::string check_orphans_text = "Check for orphaned files (files found on local filesystem that are not found on GOG servers). Sets regular expression filter (Perl syntax) for files to check. If no argument is given then the regex defaults to '" + orphans_regex_default + "'";
 
     // Help text for subdir options
-    std::string subdir_help_text = "\nTemplates:\n- %platform%\n- %gamename%\n- %gamename_firstletter%\n- %dlcname%\n- %gamename_transformed%\n- %gamename_transformed_firstletter%";
+    std::string subdir_help_text = "\nTemplates:\n"
+        "- %platform%\n"
+        "- %gamename%\n"
+        "- %gamename_firstletter%\n"
+        "- %dlcname%\n"
+        "- %gamename_transformed%\n"
+        "- %gamename_transformed_firstletter%\n"
+        "- %title%\n"
+        "- %title_stripped%\n"
+        "- %dlc_title%\n"
+        "- %dlc_title_stripped%";
 
     // Help text for include and exclude options
     std::string include_options_text;

commit 0bae0c2160f7c2a8d9dbbcf38f9e542e18661cf7

Fix orphan check
Fixes orphan check being broken after changes in 367b57ca8905dde5d4a5ba7d97fa9aa9754863b3

include/gamedetails.h
@@ -49,12 +49,12 @@ class gameDetails
         std::vector<gameFile> getGameFileVector();
         std::vector<gameFile> getGameFileVectorFiltered(const unsigned int& iType);
         void filterWithType(const unsigned int& iType);
+        std::string makeCustomFilepath(const std::string& filename, const gameDetails& gd, const DirectoryConfig& dirConf);
         virtual ~gameDetails();
     protected:
         void filterListWithPriorities(std::vector<gameFile>& list, const gameSpecificConfig& config);
         void filterListWithType(std::vector<gameFile>& list, const unsigned int& iType);
         std::string makeFilepath(const gameFile& gf, const DirectoryConfig& dirConf);
-        std::string makeCustomFilepath(const std::string& filename, const gameDetails& gd, const DirectoryConfig& dirConf);
     private:
         std::string serialsFilepath;
         std::string logoFilepath;

include/util.h
@@ -67,7 +67,6 @@ namespace Util
     int getGameSpecificConfig(std::string gamename, gameSpecificConfig* conf, std::string directory = std::string());
     int replaceString(std::string& str, const std::string& to_replace, const std::string& replace_with);
     int replaceAllString(std::string& str, const std::string& to_replace, const std::string& replace_with);
-    void filepathReplaceReservedStrings(std::string& str, const std::string& gamename, const unsigned int& platformId = 0, const std::string& dlcname = "");
     void setFilePermissions(const boost::filesystem::path& path, const boost::filesystem::perms& permissions);
     int getTerminalWidth();
     void getManualUrlsFromJSON(const Json::Value &root, std::vector<std::string> &urls);

src/downloader.cpp
@@ -1712,8 +1712,7 @@ void Downloader::checkOrphans()
             }
             for (unsigned int j = 0; j < platformIds.size(); ++j)
             {
-                std::string directory = config.dirConf.sDirectory + "/" + config.dirConf.sGameSubdir + "/";
-                Util::filepathReplaceReservedStrings(directory, games[i].gamename, platformIds[j]);
+                std::string directory = games[i].makeCustomFilepath("", games[i], config.dirConf);
                 boost::filesystem::path path (directory);
                 if (boost::filesystem::exists(path))
                 {

src/util.cpp
@@ -371,70 +371,10 @@ int Util::replaceAllString(std::string& str, const std::string& to_replace, cons
 
     do {
         str.replace(str.begin()+pos, str.begin()+pos+to_replace.length(), replace_with);
-
-        pos = str.find(to_replace, pos + to_replace.length());
+        pos = str.find(to_replace);
     } while(pos != std::string::npos);
 
     return 1;
-}
-
-void Util::filepathReplaceReservedStrings(std::string& str, const std::string& gamename, const unsigned int& platformId, const std::string& dlcname)
-{
-    std::string platform;
-    for (unsigned int i = 0; i < GlobalConstants::PLATFORMS.size(); ++i)
-    {
-        if ((platformId & GlobalConstants::PLATFORMS[i].id) == GlobalConstants::PLATFORMS[i].id)
-        {
-            platform = boost::algorithm::to_lower_copy(GlobalConstants::PLATFORMS[i].str);
-            break;
-        }
-    }
-    if (platform.empty())
-    {
-        if (str.find("%gamename%/%platform%") != std::string::npos)
-            platform = "";
-        else
-            platform = "no_platform";
-    }
-
-    // Don't save certain files in "no_platform" folder
-    if (
-           str.rfind("/icon.png") != std::string::npos
-        || str.rfind("/logo.jpg") != std::string::npos
-        || str.rfind("/product.json") != std::string::npos
-    )
-        platform = "";
-
-    std::string gamename_firstletter;
-    if (!gamename.empty())
-    {
-        if (std::isdigit(gamename.front()))
-            gamename_firstletter = "0";
-        else
-            gamename_firstletter = gamename.front();
-    }
-
-    if (str.find("%gamename_transformed%") != std::string::npos || str.find("%gamename_transformed_firstletter%") != std::string::npos)
-    {
-        std::string gamename_transformed = transformGamename(gamename);
-        std::string gamename_transformed_firstletter;
-        if (!gamename_transformed.empty())
-        {
-            if (std::isdigit(gamename_transformed.front()))
-                gamename_transformed_firstletter = "0";
-            else
-                gamename_transformed_firstletter = gamename_transformed.front();
-        }
-
-        while (Util::replaceString(str, "%gamename_transformed%", gamename_transformed));
-        while (Util::replaceString(str, "%gamename_transformed_firstletter%", gamename_transformed_firstletter));
-    }
-
-    while (Util::replaceString(str, "%gamename_firstletter%", gamename_firstletter));
-    while (Util::replaceString(str, "%gamename%", gamename));
-    while (Util::replaceString(str, "%dlcname%", dlcname));
-    while (Util::replaceString(str, "%platform%", platform));
-    while (Util::replaceString(str, "//", "/")); // Replace any double slashes with single slash
 }
 
 void Util::setFilePermissions(const boost::filesystem::path& path, const boost::filesystem::perms& permissions)