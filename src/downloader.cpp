Sude-/lgogdownloader

commit 7653a52631697138b3099f49e6267f95249bc100

Galaxy: Sort Galaxy builds
Sort Galaxy builds by date and branch
Deprioritize branches and deprioritize "compatibility" branch even more

src/downloader.cpp
@@ -4077,6 +4077,7 @@ void Downloader::galaxyInstallGameById(const std::string& product_id, const std:
         sPlatform = "windows";
 
     Json::Value json = gogGalaxy->getProductBuilds(product_id, sPlatform);
+    json = this->sortGalaxyProductBuilds(json, Globals::globalConfig.sGalaxyBuildSortingOrder);
 
     // JSON is empty and platform is Linux. Most likely cause is that Galaxy API doesn't have Linux support
     if (json.empty() && iPlatform == GlobalConstants::PLATFORM_LINUX)
@@ -4399,6 +4400,7 @@ void Downloader::galaxyListCDNsById(const std::string& product_id, const std::st
         sPlatform = "windows";
 
     Json::Value json = gogGalaxy->getProductBuilds(product_id, sPlatform);
+    json = this->sortGalaxyProductBuilds(json, Globals::globalConfig.sGalaxyBuildSortingOrder);
 
     // JSON is empty and platform is Linux. Most likely cause is that Galaxy API doesn't have Linux support
     if (json.empty() && iPlatform == GlobalConstants::PLATFORM_LINUX)
@@ -4876,6 +4878,7 @@ void Downloader::galaxyShowBuildsById(const std::string& product_id, const std::
         sPlatform = "windows";
 
     Json::Value json = gogGalaxy->getProductBuilds(product_id, sPlatform);
+    json = this->sortGalaxyProductBuilds(json, Globals::globalConfig.sGalaxyBuildSortingOrder);
 
     // JSON is empty and platform is Linux. Most likely cause is that Galaxy API doesn't have Linux support
     if (json.empty() && iPlatform == GlobalConstants::PLATFORM_LINUX)
@@ -5071,6 +5074,7 @@ std::map<std::string, std::string> Downloader::cloudSaveLocations(const std::str
         sPlatform = "windows";
 
     Json::Value json = gogGalaxy->getProductBuilds(product_id, sPlatform);
+    json = this->sortGalaxyProductBuilds(json, Globals::globalConfig.sGalaxyBuildSortingOrder);
 
     int build_index = this->galaxyGetBuildIndexWithBuildId(json, build_id);
     build_index = std::max(0, build_index);
@@ -6844,4 +6848,79 @@ void Downloader::printGameFileDetailsAsText(gameFile& gf)
         std::cout << "\tversion: " << gf.version << std::endl;
 
     std::cout << std::endl;
+}
+
+Json::Value Downloader::sortGalaxyProductBuilds(Json::Value json, const std::string& sorting_order)
+{
+    if (sorting_order.empty() || sorting_order == "none")
+        return json;
+
+    std::vector<Json::Value> vItems;
+    for (auto item : json["items"])
+        vItems.push_back(item);
+
+    if(sorting_order == "date")
+    {
+        // Sort by date
+        std::sort(vItems.begin(), vItems.end(), [](const Json::Value& a, const Json::Value& b)
+            {
+                return a["date_published"].asString() > b["date_published"].asString();
+            }
+        );
+
+        Json::Value sorted_json;
+        for (auto item : vItems)
+            sorted_json["items"].append(item);
+
+        json["items"] = sorted_json["items"];
+    }
+    else if (sorting_order == "score")
+    {
+        // Sort by date
+        std::sort(vItems.begin(), vItems.end(), [](const Json::Value& a, const Json::Value& b)
+            {
+                return a["date_published"].asString() > b["date_published"].asString();
+            }
+        );
+
+        // Apply score to items
+        typedef struct { Json::Value json; int score; } scoredItem;
+        std::vector<scoredItem> vScoredItems;
+        for (unsigned int i = 0; i < vItems.size(); i++)
+        {
+            scoredItem item;
+            item.json = vItems[i];
+            int score = i; // Set score to item index
+
+            // Check if item belongs to any branch
+            std::string branch = item.json["branch"].asString();
+            if (!branch.empty())
+            {
+                // Deprioritize item if it belongs to any branch
+                score += vItems.size();
+
+                // Deprioritize even more if it belongs to "compatibility" branch
+                if (branch == "compatibility")
+                    score += vItems.size();
+            }
+
+            item.score = score;
+            vScoredItems.push_back(item);
+        }
+
+        // Sort by score
+        std::sort(vScoredItems.begin(), vScoredItems.end(), [](const scoredItem& a, const scoredItem& b)
+            {
+                return a.score < b.score;
+            }
+        );
+
+        Json::Value sorted_json;
+        for (auto item : vScoredItems)
+            sorted_json["items"].append(item.json);
+
+        json["items"] = sorted_json["items"];
+    }
+
+    return json;
 }

include/config.h
@@ -305,6 +305,7 @@ class Config
         std::string sVersionNumber;
         std::string sEmail;
         std::string sPassword;
+        std::string sGalaxyBuildSortingOrder;
 
         // Lists
         Blacklist blacklist;

include/downloader.h
@@ -178,6 +178,7 @@ class Downloader
         bool galaxySelectProductIdHelper(const std::string& product_id, std::string& selected_product);
         std::vector<galaxyDepotItem> galaxyGetDepotItemVectorFromJson(const Json::Value& json, const unsigned int& iGalaxyArch = GlobalConstants::ARCH_X64);
         int galaxyGetBuildIndexWithBuildId(Json::Value json, const std::string& build_id = std::string());
+        Json::Value sortGalaxyProductBuilds(Json::Value json, const std::string& sorting_order = "none");
 
         Website *gogWebsite;
         galaxyAPI *gogGalaxy;

main.cpp
@@ -144,6 +144,12 @@ int main(int argc, char *argv[])
         "- %dlc_title%\n"
         "- %dlc_title_stripped%";
 
+    // Help text for subdir options
+    std::string galaxy_sort_help_text = "\nOptions:\n"
+        "\"none\" - no sorting\n"
+        "\"date\" - sort by date\n"
+        "\"score\" - sort by date and deprioritize branches, deprioritizes compatibility branches even more\n";
+
     // Help text for include and exclude options
     std::string include_options_text;
     for (unsigned int i = 0; i < GlobalConstants::INCLUDE_OPTIONS.size(); ++i)
@@ -303,6 +309,7 @@ int main(int argc, char *argv[])
         options_cli_no_cfg_hidden.add_options()
             ("login-email", bpo::value<std::string>(&Globals::globalConfig.sEmail)->default_value(""), "login email")
             ("login-password", bpo::value<std::string>(&Globals::globalConfig.sPassword)->default_value(""), "login password")
+            ("galaxy-builds-sort", bpo::value<std::string>(&Globals::globalConfig.sGalaxyBuildSortingOrder)->default_value("score"), ("Sorting order for Galaxy builds" + galaxy_sort_help_text).c_str())
         ;
 
         options_cli_experimental.add_options()

commit be7a4110fe2761075cc869528c3da17d9a66a724

Galaxy: Fix --galaxy-install and --galaxy-show-builds for Linux games
The changes made in 7653a52631697138b3099f49e6267f95249bc100 broke --galaxy-install and --galaxy-show-builds for Linux games
Product builds json is empty for Linux games but Downloader::sortGalaxyProductBuilds didn't check for empty json and proceeded to add empty items array to it.
This broke a check later in the code because the json wasn't empty anymore.

src/downloader.cpp
@@ -6859,7 +6859,7 @@ void Downloader::printGameFileDetailsAsText(gameFile& gf)
 
 Json::Value Downloader::sortGalaxyProductBuilds(Json::Value json, const std::string& sorting_order)
 {
-    if (sorting_order.empty() || sorting_order == "none")
+    if (sorting_order.empty() || sorting_order == "none" || json.empty())
         return json;
 
     std::vector<Json::Value> vItems;